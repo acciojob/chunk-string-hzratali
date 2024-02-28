0f149982a1971b0bef9dfca02c63fc89b53eaac2
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-02-28 17:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function stringChop(str, size) {
   // your code here
+	if (!str) return []; // Check if input string is null or empty
 	const chunks = [];
 	for (let i = 0; i < str.length; i += size) {
 		chunks.push(str.slice(i, i + size));
@@ -8,6 +9,6 @@ function stringChop(str, size) {
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
+// alert(stringChop(str, size));
